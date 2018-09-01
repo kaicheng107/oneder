703e58ecab7a95f82bf167d9fd88bfbd04660d85
KC <kaicheng107@hotmail.com>
2018-09-01 14:02:04 +0800

    netbean New

---
+++ onederNetBean/onederNetBean-ejb/src/java/stateless/peopleSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stateless;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Kai Cheng
+ */
+@Stateless
+public class peopleSessionBean implements peopleSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
